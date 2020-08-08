4ce5557eb92c1956045e39cd13806210aa96f320
syber3048 <vitorferreira.braga@hotmail.com>
2020-08-08 16:44:40 -0300

    bugs fixed

--- main.js
+++ main.js
@@ -1,110 +1,108 @@
-	var urso = document.getElementById("img-bolinha");
+//declarar o objeto
+const bolinha = document.getElementById("bolinha");
+bolinha.style = "margin-top: 255px; margin-left:239px;";//239
 
-	var bolinha = [
-		'bolinha1.png',	//0
-		'bolinha1.png'		//1
-	]
+ 
 
-	var quadro = 0;  	//index de quadros
+//DEFININDO LIMITES
+const baixo = 255;
+const baixodireita = 460;
+const baixoesquerda = 30;
+const cima = 100;
+const cimadireita = 380;
+const cimaesquerda = 110;
+const direita = 490;
+const esquerda = 5;
 
-	function roda(){
-		setTimeout(animar,3000);// comandos, tempo em milesegundos
-		//setTimeout -> faz uma contagem regressiva para executar um comando
-	}
+ 
 
-var position = 227;
-	var positionv = 233;
-	var positiontrás=480;
-	//var position=480;
-	//var positionv =207;
-	urso.style = "margin-top:"+positionv+"px; margin-left:"+position+"px;margin-right:"+positiontrás+"px;";		
-	var direction = 0; // 0 FRENTE, 1 TRÁS
+//variáveis para os eixos
+var positionv = 255;
+var positionh = 239;
 
-	function animar(){
-		
-		timer = window.setInterval(function(){ //executa os comandos a cada intervalo
-			//ANIMA O URSO - TROCA DE QUADROS
-			urso.src = bolinha[quadro];
-			quadro++;
-			if(quadro > 1){
-				quadro = 0;
-			};//FIM ANIMA URSO
+ 
 
-			
+//DIRECIONAMENTO
+var directionh = 0;// 0 para frente, 1 para trás
+var directionv = 0;// 0 para cima, 1 para baixo
 
-			//MOVIMENTA O URSO
-			var largura = 498-45;//guardei a largura disponível
-			
-      
-			if (direction == 0 && position<largura){
-				position += 5;
-			}
-		
-    else if (direction == 0  && largura==453  ){
-			position += 5; //margin-left
-			positionv -= 4;//margin-top
-		} 
-		else if( direction==1 &&  largura==498){
-			position-=5;
-		
-		}
-		
+ 
 
-		document.getElementById("img-bolinha").style = "margin-top:"+positionv
-                                        +"px; margin-left:"+position+"px";
-			
-																				
-		/*	var direction= 0;																	
-			if (direction === 0 && position<largura){
-				position += 9; //margin-left
-				positionv -= 4;//margin-top
-			} 
+function anda(){
+    setInterval(function(){//começa a função de animação
+        //percorrer reta de largada
+        if(        
+            directionh == 0 &&
+             positionv >= 255 &&
+             positionh < baixodireita
+             ){
 
-			urso.style = "margin-top:"+positionv
-						+"px; margin-left:"+position+"px;";
-						*/
-     
-			}, 200);
-		}		
-		
+ 
 
-		
+            positionh += 5;
+        } //fim da reta de largada
+        else if( //primeiracurva
+                directionh == 0 &&
+                positionv > 200 && 
+                positionh >= baixodireita &&
+                positionh <= direita
+                ){
+            positionh +=5;
+            positionv -=5;
 
-	function paraurso(){
-		clearInterval(timer); //clearTimeout(variavel do timer)
-		document.getElementById("img-urso").src = "urso1.png";
-	}	
-	//faça o urso andar até o final da janela e voltar de moonwalker
+ 
 
+            if(positionh>=direita){ //ao cehgar no meio da curva
+            directionh = 1;
+            }//passa a andar para trás (ESQUERDA)
+        
+        } //fim da primeira curva primeiracurva
+        else if( //segunda reta
+                directionh === 1 &&
+                positionh >= cimadireita &&
+                positionv >= cima
+            ){
 
+ 
 
-	//declaração de variáveis em JS
-	/*
-	variavel; //sem declaração de escopo a variável é criada no escopo GLOBAL
-	let seila; //variável de escopo
+            positionv -= 7;
+            positionh -= 5.5;
+        } // fim da segunda reta
+        else if( //reta oposta
+                directionh === 1 &&
+                positionh >= cimaesquerda
+                ){
+            positionh -= 5;
+        } //fim da reta oposta
+        else if(//ultima reta
+                directionh === 1 &&
+                positionh <= cimaesquerda &&
+                positionh >= esquerda                        
+               ){
+            positionh -= 7;
+            positionv += 8;
 
-	for(let i = 0; i < 10 ; i++){
-		variavel2;
-	}
+ 
 
-	var outraVariavel;
+            if(positionh <= esquerda){//terminando a curva voltar para frente
+                directionh = 0;
+            }
+        } //fim da ultima reta
+        else{
+            positionh += 5;
+            positionv += 5;
 
-	let seila = 0;
+ 
 
-	var outraVariavel = 1;
+        }
+                
 
-	var variavel2;
+ 
 
-	outravariavel = 2;
+        bolinha.style = "margin-top:"+positionv+"px;"+
+                        "margin-left:"+positionh+"px;";
 
-	"sair" == "SAIR" true
-	"sair" != "SAIR" false
-	"sair" === "SAIR" false
-	"sair" !== "SAIR" true
-	0 == "0" true
-	0 === "0" false
-	"a" == 'a' true
-	"a" === 'a' false
-	!= !==
+ 
 
-	*/
+    }/*termina função de animação*/,100);
+}
